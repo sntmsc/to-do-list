4440b4c10a86a4e095b7b3221a01cc0ef7122dcb
Santiago Hamber <80355751+sntmsc@users.noreply.github.com>
2021-10-17 22:00:49 -0300

    Update Tarea.jsx

--- src/components/Tarea.jsx
+++ src/components/Tarea.jsx
@@ -25,7 +25,7 @@ export const Tarea = ({x,i,tareas,cambiarTareas,setMensajeAlerta,mostrarAlerta})
 
     return(
         <Flex as={motion.div} key={x.i} boxShadow="dark-lg" align="center" borderRadius="5px" p={1} 
-        bg="gray.300" width="100%" mt={1} initial={{scale: 0}} animate={{scale: 1}} exit={{scale: 0}} transition={{scale:{duration: .5}}}>
+        bg="gray.300" width="100%" mt={1} initial={{scale: 0.9}} animate={{scale: 1}} exit={{scale: 0.9}} transition={{scale:{duration: 0.5}}}>
             <Box p={2} > 
             {!edicionVisible &&
               <Text fontFamily="'PT Sans Narrow', sans-serif;" fontSize="1.5em" >{x.tarea}</Text>
